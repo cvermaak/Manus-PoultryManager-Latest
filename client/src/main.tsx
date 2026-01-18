8316cfc8736f8e88a3aa4e9280e773afe12cdbd6
Clinton <clinton.vermaak@gmail.com>
2026-01-18 17:33:49 +0200

    Fix URL issues

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -40,7 +40,7 @@ queryClient.getMutationCache().subscribe(event => {
 const trpcClient = trpc.createClient({
   links: [
     httpBatchLink({
-      url: "/api/trpc",
+      url: import.meta.env.VITE_API_URL || `${window.location.origin}/api/trpc`,
       transformer: superjson,
       fetch(input, init) {
         return globalThis.fetch(input, {
